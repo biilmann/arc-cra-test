038ce82291a24e47c11623e2e5d48442f9cda360
Brian LeRoux <b@brian.io>
2018-11-09 18:22:33 -0800

    addsssss cats I guess

--- api/src/http/post-api-cats/index.js
+++ api/src/http/post-api-cats/index.js
@@ -1,47 +1,14 @@
-// @architect/functions enables secure sessions, express-style middleware and more
-// let arc = require('@architect/functions')
-// let url = arc.http.helpers.url
+let arc = require('@architect/functions')
+let data = require('@architect/data')
+let url = arc.http.helpers.url
 
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    type: 'text/html; charset=utf8',
-    body: '<h1>Hello world!</h1>'
-  }
-}
-
-// Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http(request) {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(request)
-  }
-  return {
-    status: 302,
-    location: '/staging/about',
-  }
-}
-*/
-
-/* Successful resource creation, CORS enabled
-exports.handler = async function http(request) {
+  let cat = Object.assign(req.body, {catID:''+Date.now()})
+  await data.cats.put(cat)
   return {
     status: 201,
-    type: 'application/json',
-    body: JSON.stringify({ok: true}),
-    cors: true,
+    location: url(`/api/cats/${cat.catID}`),
+    body: '{}'
   }
 }
-*/
 
-/* Deliver client-side JS
-exports.handler = async function http(request) {
-  return {
-    type: 'text/javascript',
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
-
-// Learn more: https://arc.codes/guides/http
